0c743bae74dd5b188c992311e4fb08e42c51c568
OPHSC2009 <92368269+OPHSC2009@users.noreply.github.com>
2023-04-05 23:19:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+
+function startClassification(){
+navigator.mediaDevices.getUserMedia({audio:true})
+classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/p-pYTxQ1G/model.json",modelReady);
+}
+
+function modelReady(){
+classifier.classify(gotResults);
+
+}
+
+function gotResults(error,results){
+if(error){
+    console.log(error)
+}
+else{
+    console.log(results)
+}
+
+}
